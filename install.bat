ff1d46b8848d9174565ded88710dc822036b7f76
Šebestíček <44745014+SebestikCZ@users.noreply.github.com>
2021-07-24 22:37:02 +0200

    Updated modules to installation

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 call npm init -y
-call npm i discord.js ffmpeg-static Garlic-Team/GCommands#dev opusscript solenolyrics youtube-sr ytdl-core
+call npm i discord.js ffmpeg-static Garlic-Team/GCommands#dev opusscript solenolyrics youtube-sr ytdl-core dotenv erit-ytdl express
 echo Everything setup!
 pause
