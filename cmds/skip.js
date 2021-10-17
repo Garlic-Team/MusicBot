7b7cc77cc58cae983d94f44864173ecaf60fb3b6
DamFerr <76624500+DamFerr@users.noreply.github.com>
2021-10-17 18:56:01 +0200

    Update skip.js

--- cmds/skip.js
+++ cmds/skip.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: "skip",
     description: "Skips the current song",
-    alises: ["sk","n"],
+    aliases: ["sk","n"],
     clientRequiredPermissions: ["SEND_MESSAGES"],
     run: async({client, interaction, respond, guild, edit, member}, args) => {
       let error = (c) => respond({ content: `:x: *${c}*`, ephemeral: true });
